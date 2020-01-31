11ea8c1179678ef7bd5267f762b031778f33abed
Rui <lirui.jobs@gmail.com>
2020-02-01 00:58:55 +1100

    Updates

---
+++ precache-manifest.4a751768ac2c1c0e89fc99eba67aa76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce23467b2685fc8eb9e6a69d862115bf",
+    "url": "/DailyExpense2/index.html"
+  },
+  {
+    "revision": "f945de9b5e483b4e5ed4",
+    "url": "/DailyExpense2/static/css/main.de7cd6b1.chunk.css"
+  },
+  {
+    "revision": "f911e83ca6d6d860eaba",
+    "url": "/DailyExpense2/static/js/2.b7e52a55.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/DailyExpense2/static/js/2.b7e52a55.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f945de9b5e483b4e5ed4",
+    "url": "/DailyExpense2/static/js/main.4a9c376a.chunk.js"
+  },
+  {
+    "revision": "8055b3daf340e54845c4",
+    "url": "/DailyExpense2/static/js/runtime-main.8789c662.js"
+  }
+]);
