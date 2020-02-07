5e064ee2e748ae7e5342594a9e486cf8f7139e36
Rui Li <lirui.jobs@gmail.com>
2020-02-07 11:08:16 +1100

    Updates

---
+++ precache-manifest.699de847b6fc89434fb4bdc082f734d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ffaf4dcbf04fc87e29f5531a5f45c1",
+    "url": "/DailyExpense2/index.html"
+  },
+  {
+    "revision": "09e6f48dad6074bb2981",
+    "url": "/DailyExpense2/static/css/main.430541b5.chunk.css"
+  },
+  {
+    "revision": "51bd7ef81b3f99042505",
+    "url": "/DailyExpense2/static/js/2.2e56a7eb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/DailyExpense2/static/js/2.2e56a7eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09e6f48dad6074bb2981",
+    "url": "/DailyExpense2/static/js/main.658029d5.chunk.js"
+  },
+  {
+    "revision": "8055b3daf340e54845c4",
+    "url": "/DailyExpense2/static/js/runtime-main.8789c662.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DailyExpense2/precache-manifest.c5564e50f27a067738a34f9622601021.js"
+  "/DailyExpense2/precache-manifest.699de847b6fc89434fb4bdc082f734d8.js"
 );
 
 self.addEventListener('message', (event) => {
